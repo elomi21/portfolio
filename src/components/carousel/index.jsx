9a9e7b82b09cbbd1d43cd7b45c9ee6f987f00b82
Elodie Mitaut <mitautelodie@gmail.com>
2024-04-29 12:05:15 +0200

    carousel fonctionnel

--- src/components/carousel/index.jsx
+++ src/components/carousel/index.jsx
@@ -1,60 +1,57 @@
+import { useState } from "react";
+import Arrowleft from "../../assets/arrowL.svg";
+import Arrowright from "../../assets/arrowR.svg";
+import Card from "../card/card";
+import Projetsdata from "../../projetsdata.json";
 
-// import { useState } from "react";
-// import Arrowleft from "../../assets/arrowL.svg";
-// import Arrowright from "../../assets/arrowR.svg";
+function Carousel() {
+  const projetsdata = Projetsdata;
+  const [currentCard, setCurrentCard] = useState(0);
+  const totalCards = projetsdata.length;
 
-function Carousel({ pictures, alt, cover }) {
-//   const [curentPicture, setCurentPicture] = useState(0);
-//   const totalPictures = pictures.length;
-
-//   /* fonction pour l'affichage des photos au clic sur la fléche de gauche*/
-//   function PreviousPicture() {
-//     curentPicture > 0
-//       ? setCurentPicture(curentPicture - 1)
-//       : setCurentPicture(totalPictures - 1);
-//   }
-//   /* fonction pour l'affichage des photos au clic sur la fléche de droite*/
-//   function NextPictures() {
-//     curentPicture < totalPictures - 1
-//       ? setCurentPicture(curentPicture + 1)
-//       : setCurentPicture(0);
-//   }
+  /* fonction pour l'affichage des photos au clic sur la fléche de gauche*/
+  function PreviousCard() {
+    currentCard > 0
+      ? setCurrentCard(currentCard - 1)
+      : setCurrentCard(totalCards - 1);
+  }
+  /* fonction pour l'affichage des photos au clic sur la fléche de droite*/
+  function NextCard() {
+    currentCard < totalCards - 1
+      ? setCurrentCard(currentCard + 1)
+      : setCurrentCard(0);
+  }
 
   return (
     <>
-      {/* <div className="container-carousel">
-        {pictures.length === 0 ? (
-          <img className="k-sl-slide" src={cover} alt={alt} />
-        ) : pictures.length === 1 ? (
-          <img className="k-sl-slide" src={pictures[curentPicture]} alt={alt} />
-        ) : (
-          <div>
-            <img
-              onClick={PreviousPicture}
-              className="k-sl-al"
-              src={Arrowleft}
-              alt="arrow_left"
-            />
-
-            <img
-              className="k-sl-slide"
-              src={pictures[curentPicture]}
-              alt={alt}
-            />
-            <span className="k-sl-counter">
-              {curentPicture + 1}/{totalPictures}
-            </span>
-            <img
-              onClick={NextPictures}
-              className="k-sl-ar"
-              src={Arrowright}
-              alt="arrow_rigth"
-            />
-          </div>
-        )}
-      </div> */}
+      <section className="carousel">
+        <div className="container-carousel">
+          <img
+            onClick={PreviousCard}
+            className="c-sl-al"
+            src={Arrowleft}
+            alt="arrow_left"
+          />
+          <Card
+            id={projetsdata[currentCard].id}
+            title={projetsdata[currentCard].title}
+            cover={projetsdata[currentCard].cover}
+            langages={projetsdata[currentCard].langages}
+          />
+          <img
+            onClick={NextCard}
+            className="c-sl-ar"
+            src={Arrowright}
+            alt="arrow_rigth"
+          />
+        </div>
+        <div className="c-sl-counter">
+          {currentCard + 1}/{totalCards}
+        </div>
+      </section>
     </>
   );
 }
+
 
 export default Carousel;
