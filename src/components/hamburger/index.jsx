0419d326d6d6649928dc326ed321e1671d38b1f5
Elodie Mitaut <mitautelodie@gmail.com>
2024-05-13 20:05:16 +0200

    modif json ajout kasa description

--- src/components/hamburger/index.jsx
+++ src/components/hamburger/index.jsx
@@ -1,9 +1,9 @@
 function Hamburger() {
   return (
     <div className="hamburger">
-      <div className="burger burger-open burger1" />
-      <div className="burger burger-open burger2" />
-      <div className="burger burger-open burger3" />
+      <div className="burger" />
+      <div className="burger" />
+      <div className="burger" />
     </div>
   );
 }
